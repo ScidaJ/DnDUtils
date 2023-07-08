aafa7a7cf559078309da624bdc8506de4cf44c42
ScidaJ <scidaj@gmail.com>
2023-07-08 19:26:37 -0400

    Same thing but in the user model

--- api/models/user_model.go
+++ api/models/user_model.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id        primitive.ObjectID `json:"id,omitempty" validate:"required"`
+	Id        primitive.ObjectID `json:"id,omitempty" bson:"_idomitempty" validate:"required"`
 	DiscordId string             `json:"discord_id,omitempty" validate:"required"`
 	Servers   []string           `json:"servers,omitempty" validate:"required"`
 	Owns      []string           `json:"owns,omitempty" validate:"required"`
