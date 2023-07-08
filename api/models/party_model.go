8925ef48dac420683d1a3767214072c6fa735f8d
ScidaJ <scidaj@gmail.com>
2023-07-08 19:24:38 -0400

    Fixed bug where ID field was being duplicated

--- api/models/party_model.go
+++ api/models/party_model.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Party struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
+	Id       primitive.ObjectID `json:"id" bson:"_id"`
 	Name     string             `json:"name,omitempty" validate:"required"`
 	ServerId string             `json:"server_id,omitempty" validate:"required"`
 	Owner    string             `json:"owner,omitempty" validate:"required"`
